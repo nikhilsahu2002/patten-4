fe393927a0cc52668e0a7af61befd145fea344e7
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-12-19 21:49:30 +0530

    Add files via upload

---
+++ PATTEN.C
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<conio.h>
+
+int i,j,count=1,k;
+
+/*
+void patten()
+{
+
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+
+}
+void patten2()
+{
+	for(i=1;i<=5;i++)
+	{
+		for(j=5;j>=i;j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+void patten3()
+{
+
+	for(i=0;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",count);
+			count++;
+		}
+		printf("\n");
+	}
+
+}         */
+
+void patten4()
+{
+	for(i=5;i>=0;i--)
+	{
+		for(j=0;j<=i;j++)
+		{
+			printf(" ");
+
+		}
+		for(k=1;k<=count;k++)
+		{
+		printf("*");
+		}
+		printf("\n");
+		count++;
+
+	}
+}
+void main()
+{
+clrscr();
+patten4();
+}
